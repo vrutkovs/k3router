ce394459053a8adf82b64141d873cd9a221f2995
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-06-25 09:56:58 +0200

    Initial commit
    bootstrap will install ArgoCD from OLM, configure it to listen to this repo.
    apps dir contains a list of apps.
    apps/routernetes contains configuration for routernetes operator

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+/usr/local/bin/kubectl apply -f https://operatorhub.io/install/argocd-operator.yaml
+/usr/bin/sleep 30
+/usr/local/bin/kubectl wait --timeout=24h --for=jsonpath='{.status.state}'=AtLatestKnown -n operators sub/my-argocd-operator
+csv=$(/usr/local/bin/kubectl get -n operators sub/my-argocd-operator -o jsonpath='{.status.installedCSV}')
+/usr/bin/sleep 30
+/usr/local/bin/kubectl wait --timeout=24h --for=jsonpath='{.status.phase}'=Succeeded -n operators csv/$csv
+/usr/local/bin/kubectl apply -f https://raw.githubusercontent.com/vrutkovs/gitops-routernetes/main/bootstrap.yaml
